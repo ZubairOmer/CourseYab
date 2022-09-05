ad0698edc2583588e18b3c577f70d761d5a4fae5
Zubair <omer.zubair01@gmail.com>
2022-09-05 16:50:10 +0430

    create course page completed and started working on course image upload to cloudinary

--- components/instructor/CreateCourse.js
+++ components/instructor/CreateCourse.js
@@ -1,11 +1,48 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
+import InstructorRoute from "../routes/InstructorRoute";
+import CourseCreateForm from "../forms/CourseCreateForm";
 
-const CreateCourse = () => {
+const CourseCreate = () => {
+  // state
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "9.99",
+    uploading: false,
+    paid: true,
+    loading: false,
+    imagePreview: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleImage = () => {
+    //
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <>
       <h1 className="jumbotron bg-warning text-center square">Create Course</h1>
+      <div className="pt-3 pb-3">
+        <CourseCreateForm
+          handleSubmit={handleSubmit}
+          handleImage={handleImage}
+          handleChange={handleChange}
+          values={values}
+          setValues={setValues}
+        />
+      </div>
+      <pre>{JSON.stringify(values, null, 4)}</pre>
     </>
   );
 };
 
-export default CreateCourse;
+export default CourseCreate;
