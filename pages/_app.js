d39a3025560561f83aa09ba0106a7b3bed4679f1
Zubair <omer.zubair01@gmail.com>
2022-09-06 14:39:22 +0430

    stable version gonna remove auth bug

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../styles/nprogress.css";
-import { useState } from "react";
 import { UserProvider } from "../context/userContext";
 import nProgress from "nprogress";
 import Router from "next/router";
+import { Provider } from "next-auth/client";
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
@@ -13,9 +13,11 @@ Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return (
+    // <Provider session={pageProps.session}>
     <UserProvider>
       <Component {...pageProps} />;
     </UserProvider>
+    // </Provider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import React, { Fragment } from "react";
 import Layout from "../components/layout/Layout";
+import { getSession, useSession } from "next-auth/client";
 
-const index = () => {
+const IndexPage = () => {
+  // const [session] = useSession();
+
+  // console.log("YAMI KHAW RA GYM", session);
+
   return (
     <Layout title="Edemy">
       <Fragment>
         <h1 className="jumbotron text-center bg-danger square">
-          Find Cute Courses Here
+          {/* Find Cute Courses Here {session && session.user.email} */}
         </h1>
       </Fragment>
     </Layout>
   );
 };
 
-export default index;
+export default IndexPage;
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+
+//   return {
+//     props: { session },
+//   };
+// }
